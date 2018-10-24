e1869a065dc7ff1b56a71f84c205eaba4bb197b6
Dennis Menace <dennisamenace@tutanota.com>
2018-10-23 20:14:44 -0600

    Added DNS seed

--- src/Stratis.Bitcoin.Networks/X42Main.cs
+++ src/Stratis.Bitcoin.Networks/X42Main.cs
@@ -143,9 +143,10 @@ namespace Stratis.Bitcoin.Networks
             this.DNSSeeds = new List<DNSSeedData>
             {
                 new DNSSeedData("seednode2.x42.tech", "seednode2.x42.tech"),
+                new DNSSeedData("tech.x42.cloud", "tech.x42.cloud"),
             };
 
-            string[] seedNodes = { "34.255.35.42", "18.223.166.44", "177.18.126.60" };
+            string[] seedNodes = { "34.255.35.42", "52.211.235.48", "96.73.27.73" };
             this.SeedNodes = ConvertToNetworkAddresses(seedNodes, this.DefaultPort).ToList();
 
             Assert(this.Consensus.HashGenesisBlock == uint256.Parse("0x04ffe583707a96c1c2eb54af33a4b1dc6d9d8e09fea8c9a7b097ba88f0cb64c4"));
